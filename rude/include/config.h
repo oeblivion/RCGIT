4b9b20d2e3e21159934c09739adb05638c00dfcb
Johannes Freyer <johannes_michael.freyer@mailbox.tu-dresden.de>
2023-10-21 14:24:12 +0200

    aktueller stand

---
+++ rude/include/config.h
@@ -0,0 +1,68 @@
+/* include/config.h.  Generated automatically by configure.  */
+/* autoconf/config.hin.  Generated automatically from configure.in by autoheader 2.13.  */
+/***************************************************************************
+ * autoconfig/acconfig.h                                                   *
+ *  - is the template file for autoconfig/config.hin which is generated    *
+ *    by the autoheader command                                            *
+ *                                                                         *
+ * autoconfig/config.hin                                                   *
+ *  - is the template for include/config.h, that will be gererated by      *
+ *    the ./configure script                                               *
+ *                                                                         *
+ * include/config.h                                                        *
+ *  - is the file that has the definitions/options for the pre-processor   *
+ *                                                                         *
+ ***************************************************************************/
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define to `int' if <sys/types.h> doesn't define.  */
+/* #undef gid_t */
+
+/* Define if you have <sys/wait.h> that is POSIX.1 compatible.  */
+#define HAVE_SYS_WAIT_H 1
+
+/* Define if you need to in order for stat and other things to work.  */
+/* #undef _POSIX_SOURCE */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you can safely include both <sys/time.h> and <time.h>.  */
+#define TIME_WITH_SYS_TIME 1
+
+/* Define if your <sys/time.h> declares struct tm.  */
+/* #undef TM_IN_SYS_TIME */
+
+/* Define to `int' if <sys/types.h> doesn't define.  */
+/* #undef uid_t */
+
+/* Define if you have the gettimeofday function.  */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define if you have the munlockall function.  */
+#define HAVE_MUNLOCKALL 1
+
+/* Define if you have the select function.  */
+#define HAVE_SELECT 1
+
+/* Define if you have the sigaction function.  */
+#define HAVE_SIGACTION 1
+
+/* Define if you have the strerror function.  */
+#define HAVE_STRERROR 1
+
+/* Define if you have the <signal.h> header file.  */
+#define HAVE_SIGNAL_H 1
+
+/* Define if you have the <sys/time.h> header file.  */
+#define HAVE_SYS_TIME_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Amount of DEBUGGING output */
+#define DEBUG 7
+
+/* End of file */
