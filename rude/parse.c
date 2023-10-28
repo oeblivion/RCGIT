e55e1fd41ffff947dc5f1de88b617a44c924b42a
Johannes Freyer <johannes_michael.freyer@mailbox.tu-dresden.de>
2023-10-28 17:08:15 +0200

    eingabe der Zeitzone

--- rude/parse.c
+++ rude/parse.c
@@ -540,15 +540,17 @@ int start_time(long int d, long int mo, long int y, long int h, long int m, long
   }
   RUDEBUG7("start_time aufgerufene Werte:\n(%ld:%ld:%ld:%ld:%ld:%ld:%ld)/n",d,mo,y,h,m,s,us);
 
-  // Get the actual time zone
+  /* // Get the actual time zone
   time_t now;
   struct tm localNow;
   time(&now);
   localtime_r(&now, &localNow);
   int systemTimezoneOffset = localNow.tm_gmtoff; // Timeshift in Seconds
+ */
+
 
   // calculate start as unix
-  tester_start.tv_sec = mktime(&start_tm) + systemTimezoneOffset; 
+  tester_start.tv_sec = mktime(&start_tm) - 7200; 
   tester_start.tv_usec = us;
 
   // convert the start time to UTC
